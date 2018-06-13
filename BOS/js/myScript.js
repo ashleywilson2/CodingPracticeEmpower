daabc5ba41abdd0b8c5faa9066305fd934b5e0e2
Ashley Wilson <ms.wilson0712@gmail.com>
2018-06-13 16:09:47 -0400

    Moon phase page styling with bootstrap

--- BOS/js/myScript.js
+++ BOS/js/myScript.js
@@ -1,18 +1,22 @@
 // JavaScript source code
-var rootURL = 'https://www.timeanddate.com/moon/phases/usa';
+var rootURL = 'https://www.timeanddate.com/moon/phases';
 
 function initMoonPhase(){
 
-
   $('#moon-phases').click(function(){
-    var locationRaw = $("#location").val();
-    var location = encodeURIComponent(locationRaw);
-    var locationURL = rootURL + '/' + location;
+
+    var locationRawCity = $("#location-city").val();
+    var locationRawCountry = $('#location-country').val();
+    $("#city-country").html(locationRawCity + ", " + locationRawCountry);
+    var locationCity = encodeURIComponent(locationRawCity);
+    var locationCountry = encodeURIComponent(locationRawCountry);
+    var locationURL = rootURL + '/' + locationCountry + '/' + locationCity;
     $.get(locationURL, function(data){
       var text = $($.parseHTML(data)).find('#mn-cyc').html();
       var cleanHTML = text.replace(/\/\/c.tadst.com/g, 'https://c.tadst.com');
       $("#display-here").html(cleanHTML);
-      $("#location").val("");
+      $("#location-city").val("");
+      $("#location-country").val("");
     });
   });
 
